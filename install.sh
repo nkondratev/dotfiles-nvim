7335217fc9e7b96c066705428bf80291336f128e
nkondratev <nikita.kondratiev01@gmail.com>
2026-04-04 11:57:50 +0300

    add uv, pyright, luarocks in install

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 wget https://github.com/neovim/neovim/releases/download/v0.12.0/nvim-linux-x86_64.appimage
 chmod +x nvim-linux-x86_64.appimage
-sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+mv nvim-linux-x86_64.appimage $HOME/.local/bin/nvim
+
+echo 'export PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc
+
+wget https://luarocks.github.io/luarocks/releases/luarocks-3.13.0-linux-x86_64.zip
+unzip luarocks-3.13.0-linux-x86_64.zip 
+rm luarocks-3.13.0-linux-x86_64.zip
+mv luarocks-3.13.0-linux-x86_64/luarocks $HOME/.local/bin/luarocks
+rm -r luarocks-3.13.0-linux-x86_64
 
 mkdir -p $HOME/.config/nvim
 cp -r * $HOME/.config/nvim/
 rm $HOME/.config/nvim/install.sh
+
+# install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv tool install pyright
