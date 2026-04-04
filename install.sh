578f35fae2ef36023e38a737bc1eca7b0e4a393b
nkondratev <nikita.kondratiev01@gmail.com>
2026-04-04 11:24:53 +0300

    fix install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 wget https://github.com/neovim/neovim/releases/download/v0.12.0/nvim-linux-x86_64.appimage
-sudo mv nvim-linux-x86_64 /usr/local/bin/nvim
+chmod +x nvim-linux-x86_64.appimage
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
 
 mkdir -p $HOME/.config/nvim
-mv * $HOME/.config/nvim/
+cp -r * $HOME/.config/nvim/
 rm $HOME/.config/nvim/install.sh
